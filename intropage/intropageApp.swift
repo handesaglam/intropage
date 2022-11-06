5bf42756fadc5192c91b0e6d9f4d5986116af8e0
Hande SAĞLAM <63313545+handesaglam@users.noreply.github.com>
2022-11-06 18:13:52 +0300

    Initial Commit

---
+++ intropage/intropageApp.swift
@@ -0,0 +1,17 @@
+//
+//  intropageApp.swift
+//  intropage
+//
+//  Created by Hande Sağlam on 6.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct intropageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
